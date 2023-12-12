b07de4b7a7edee2f28b8788dccaa3fe4ccd0b063
MDybek <dybek8780@gmail.com>
2023-12-12 21:44:50 +0100

    Initial commit (hibernate setup)

---
+++ src/main/java/com/example/poasystentrekrutacji/PoAsystentRekrutacjiApplication.java
@@ -0,0 +1,13 @@
+package com.example.poasystentrekrutacji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoAsystentRekrutacjiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoAsystentRekrutacjiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/poasystentrekrutacji/PoAsystentRekrutacjiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poasystentrekrutacji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoAsystentRekrutacjiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
